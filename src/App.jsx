fb1a6446781a0eda5d253501204d478f75d5a017
kehinde olorunda <kehindeolorunda1000@gmail.com>
2025-09-06 10:37:19 +0100

    changes updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // src/App.jsx
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -10,19 +10,17 @@ import ThankYou from './pages/ThankYou';
 
 function App() {
   return (
-    <Router>
-      <>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/thank-you" element={<ThankYou />} />
-        </Routes>
-        <Toaster position="top-right" />
-      </>
-    </Router>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/thank-you" element={<ThankYou />} />
+      </Routes>
+      <Toaster position="top-right" />
+    </>
   );
 }
 
